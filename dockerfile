a7885b41ee7256f0d69f495ac7073974b66f3202
Alfredo <alfred.91.mc@gmail.com>
2023-09-27 18:13:46 +0200

    1

---
+++ dockerfile
@@ -0,0 +1,4 @@
+# Usa la imagen oficial de Apache FROM httpd:2.4.57
+
+# Copia los archivos de tu proyecto al directorio raíz del servidor web COPY ./ /usr/local/apache2/htdocs/
+
---
+++ ejemplo.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>mi primera pagina web en servidor</h1>
+    
+</body>
+<?php
+
+$nombre= "Alfredo";
+
+echo $nombre;
+
+$nombre="Alumno IES Jaroso";
+
+echo $nombre;
+
+?>
+</html>
+
