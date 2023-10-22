4b9dbf9b322e77f11a6c6000c41e269073c1f70e
Alfredo <alfred.91.mc@gmail.com>
2023-10-23 01:16:56 +0200

    a

--- tema3/Practica1-7/test.php
+++
@@ -1,9 +0,0 @@
-<?php
-require 'vendor/autoload.php';
-
-$pdf = new TCPDF();
-$pdf->AddPage();
-$pdf->SetFont('times', 'B', 16);
-$pdf->Cell(40, 10, 'Hello, TCPDF!');
-$pdf->Output('example.pdf', 'D');
-?>
