99b1c239cb5c21ce30d63e65ee5259cd37bf3652
Alfredo <alfred.91.mc@gmail.com>
2023-10-28 23:08:41 +0200

    1

--- tema3/Practica1-7/controlador.php
+++ tema3/Practica1-7/controlador.php
@@ -6,103 +6,26 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['email']) && isset($_PO
 
     $email=$_POST['email'];
     $password=$_POST['password'];
-
 //LLAMA A LA FUNCION QUE COMPRUEBA LA CONTRASEÑA Y AUTENTICA AL USUARIO 
         userLogin($email,$password);
 
-// SE ESTABLECE EL ARRAY
-
-    $proyectos = array(
-
-        array(
-            'id' => 1,
-            'nombre' => 'Proyecto 1',
-            'fechaInicio' => '10-11-2022',
-            'fechaFinPrevista' => '19-11-2022',
-            'diasTranscurridos' => 7,
-            'porcentajeCompletado' => 50,
-            'importancia' => 1
-        ),
-        array(
-            'id' => 2,
-            'nombre' => 'Proyecto 2',
-            'fechaInicio' => '01-12-2023',
-            'fechaFinPrevista' => '19-11-2022',
-            'diasTranscurridos' => 15,
-            'porcentajeCompletado' => 75,
-            'importancia' => 2
-        ),
-        array(
-            'id' => 3,
-            'nombre' => 'Proyecto 3',
-            'fechaInicio' => '01-12-2023',
-            'fechaFinPrevista' => '19-11-2022',
-            'diasTranscurridos' => 33,
-            'porcentajeCompletado' => 80,
-            'importancia' => 3
-        ),
-        array(
-            'id' => 4,
-            'nombre' => 'Proyecto 4',
-            'fechaInicio' => '19-11.2022',
-            'fechaFinPrevista' => '19-11-2022',
-            'diasTranscurridos' => 40,
-            'porcentajeCompletado' => 90,
-            'importancia' => 4
-        ),
-        array(
-            'id' => 5,
-            'nombre' => 'Proyecto 5',
-            'fechaInicio' => '19-11-2022',
-            'fechaFinPrevista' => '19-11-2022',
-            'diasTranscurridos' => 5,
-            'porcentajeCompletado' => 40,
-            'importancia' => 5
-        )
-
-    );
-
-// CARGA LOS PROYECTOS EN LA SESION    
-    $_SESSION['proyectos']=$proyectos;
-
 // REDIRIGE A PROYECTOS YA LOGEADO, PARA VER LOS PROYECTOS
-    header("Location: proyectos.php");
-    die();
+        header("Location: proyectos.php");
+        die();
 
 //SI PINCHAMOS EN NUEVO
-    } elseif ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['accion']) && $_POST['accion'] == 'nuevo') {
+} elseif ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['accion']) && $_POST['accion'] == 'nuevo') {
 
 //RELLENAMOS EL FORM O EL MODAL, Y AQUI SE OBTIENEN LOS DATOS
-            $nombre = $_POST['nombre'];
-            $fechaInicio = $_POST['fechaInicio'];
-            $fechaFinPrevista = $_POST['fechaFinPrevista'];
-            $diasTranscurridos = $_POST['diasTranscurridos'];
-            $porcentajeCompletado = $_POST['porcentajeCompletado'];
-            $importancia = $_POST['importancia'];
-        
-            
-//ASIGNA EL NUEVO ID AL PROYECTO
-            $proyectos = $_SESSION['proyectos'];
-            $nuevoId = max(array_column($proyectos, 'id')) + 1;
-        
-//CREA EL PROYECTO CON LOS PARAMETROS QUE HAN SIDO DEFINIDOS
-            $nuevoProyecto = array(
-                'id' => $nuevoId,
-                'nombre' => $nombre,
-                'fechaInicio' => $fechaInicio,
-                'fechaFinPrevista' => $fechaFinPrevista,
-                'diasTranscurridos' => $diasTranscurridos,
-                'porcentajeCompletado' => $porcentajeCompletado,
-                'importancia' => $importancia
-            );
-        
-// SE AGREGA EL PROYECTO AL ARRAY DE PROYECTOS
-            $_SESSION['proyectos'][] = $nuevoProyecto;
-        
-            
-// NOS REDIRIGE A PROYECTOS DONDE YA SE PUEDE VER EL PROYECTO NUEVO        
-            header("Location: proyectos.php");
-            die(); 
+    $nombre = $_POST['nombre'];
+    $fechaInicio = $_POST['fechaInicio'];
+    $fechaFinPrevista = $_POST['fechaFinPrevista'];
+    $diasTranscurridos = $_POST['diasTranscurridos'];
+    $porcentajeCompletado = $_POST['porcentajeCompletado'];
+    $importancia = $_POST['importancia'];
+
+        crearProyecto($nombre,$fechaInicio,$fechaFinPrevista,
+                        $diasTranscurridos,$porcentajeCompletado,$importancia);
 
 // SI PINCHAMOS EN GENERARPDF    
         } elseif ($_SERVER["REQUEST_METHOD"] == "GET" && isset($_GET['accion']) && $_GET['accion'] == 'generarPDF') {
@@ -128,21 +51,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['email']) && isset($_PO
             die();
             
         } elseif ($_GET['accion'] == 'eliminar' && isset($_GET['id'])) {
+
 // ELIMINAR UN PROYECTO 
-    $proyecto_id = $_GET['id'];
-
-// OBTEMENOS LOS PROYECTOS DE LA SESION
-    $proyectos = $_SESSION['proyectos'];
-
-// SE BUSCA POR ID Y SE ELIMINA DEL ARRAY
-    foreach ($proyectos as $key => $proyecto) {
-        if ($proyecto['id'] == $proyecto_id) {
-            unset($proyectos[$key]);
-            break;
-        }
-    }
-// ACTUALIZA EL ARRAY DE PROYECTOS EN LA SESION 
-    $_SESSION['proyectos'] = array_values($proyectos);
+    $proyectoId = $_GET['id'];
+            eliminarProyecto($proyectoId);
 
 // VOLVEMOS A PROYECTOS CON EL PROYECTO YA ELIMINADO
     header("Location: proyectos.php");
@@ -158,28 +70,24 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['email']) && isset($_PO
 
 // DETALLE PROYECTO        
     } elseif ($_GET['accion'] == 'ver' && isset($_GET['id'])) {
+
         $proyectoId = $_GET['id'];
-        $proyectos = $_SESSION['proyectos'];
-    
-//BUSCA EL PROYECTO ID
-        $proyectoEncontrado = null;
-        foreach ($proyectos as $proyecto) {
-            if ($proyecto['id'] == $proyectoId) {
-                $proyectoEncontrado = $proyecto;
-                break;
-            }
-        }
+
+        $proyectoEncontrado = proyectoPorId($proyectoId);
+        
         if ($proyectoEncontrado) {
 
 // ALMACENA EL PROYECTO EN UNA VARIABLE DE SESION PARA VERLO EN DETALLE EN verProyecto.php
             $_SESSION['proyectoDetalle'] = $proyectoEncontrado;
             header("Location: verProyecto.php");
             die();
+
         } else {
  // SI POR ALGUN CASUAL NO ENCUENTRA EL ID, REDIRIGE A PROYECTOS CON MENSAJE DE ERROR
-            header("Location: proyectos.php?error=proyecto_no_encontrado");
-            die();
+        header("Location: proyectos.php?error=proyecto_no_encontrado");
+        die();
         }
+        
     } else {
 
 // SI ALGUIEN INTENTA ACCEDER A CONTROLADOR.PHP SIN ENVIAR DATOS O SIN UNA ACCION VALIDA NOS RERIDIGE A LOGIN
--- tema3/Practica1-7/lib.php
+++ tema3/Practica1-7/lib.php
@@ -1,21 +1,123 @@
 <?php
 require 'vendor/autoload.php';
 
-//FUNCION PARA AUTENTICAR AL USUARIO, SE LE LLAMA DESDE CONTROLADOR
+//FUNCION PARA AUTENTICAR AL USUARIO
 function userLogin($email,$password){
     if (strlen($password) < 8 || !preg_match("/[A-Z]/",$password)){
         header("Location: login.php?error=CONTRASENA_INVALIDA");
         die();
     }
- //CARGA EL USUARIO EN LA SESION   
+    //CARGA EL USUARIO EN LA SESION   
     $_SESSION['usuario']=$email;
+
+// SE ESTABLECE EL ARRAY
+    $proyectos = array(
+
+        array(
+            'id' => 1,
+            'nombre' => 'Proyecto 1',
+            'fechaInicio' => '10-11-2022',
+            'fechaFinPrevista' => '19-11-2022',
+            'diasTranscurridos' => 7,
+            'porcentajeCompletado' => 50,
+            'importancia' => 1
+        ),
+        array(
+            'id' => 2,
+            'nombre' => 'Proyecto 2',
+            'fechaInicio' => '01-12-2023',
+            'fechaFinPrevista' => '19-11-2022',
+            'diasTranscurridos' => 15,
+            'porcentajeCompletado' => 75,
+            'importancia' => 2
+        ),
+        array(
+            'id' => 3,
+            'nombre' => 'Proyecto 3',
+            'fechaInicio' => '01-12-2023',
+            'fechaFinPrevista' => '19-11-2022',
+            'diasTranscurridos' => 33,
+            'porcentajeCompletado' => 80,
+            'importancia' => 3
+        ),
+        array(
+            'id' => 4,
+            'nombre' => 'Proyecto 4',
+            'fechaInicio' => '19-11.2022',
+            'fechaFinPrevista' => '19-11-2022',
+            'diasTranscurridos' => 40,
+            'porcentajeCompletado' => 90,
+            'importancia' => 4
+        ),
+        array(
+            'id' => 5,
+            'nombre' => 'Proyecto 5',
+            'fechaInicio' => '19-11-2022',
+            'fechaFinPrevista' => '19-11-2022',
+            'diasTranscurridos' => 5,
+            'porcentajeCompletado' => 40,
+            'importancia' => 5
+        )
+
+    );
+        // CARGA LOS PROYECTOS EN LA SESION    
+        $_SESSION['proyectos']=$proyectos;
 }
 
+//FUNCION PARA CREAR PROYECTOS
+function crearProyecto($nombre,$fechaInicio,$fechaFinPrevista,
+                        $diasTranscurridos,$porcentajeCompletado,$importancia){
 
+        $proyectos = $_SESSION['proyectos'];
+        $nuevoId = max(array_column($proyectos, 'id')) + 1;
+                        
+    //CREA EL PROYECTO CON LOS PARAMETROS QUE HAN SIDO DEFINIDOS
+        $nuevoProyecto = array(
+            'id' => $nuevoId,
+            'nombre' => $nombre,
+            'fechaInicio' => $fechaInicio,
+            'fechaFinPrevista' => $fechaFinPrevista,
+            'diasTranscurridos' => $diasTranscurridos,
+            'porcentajeCompletado' => $porcentajeCompletado,
+            'importancia' => $importancia
+        );
+                        
+                // SE AGREGA EL PROYECTO AL ARRAY DE PROYECTOS
+                            $_SESSION['proyectos'][] = $nuevoProyecto;
+                                                    
+                // NOS REDIRIGE A PROYECTOS DONDE YA SE PUEDE VER EL PROYECTO NUEVO        
+                            header("Location: proyectos.php");
+                            die(); 
+}
 
+function eliminarProyecto($proyectoId){
+    // OBTEMENOS LOS PROYECTOS DE LA SESION
+    $proyectos = $_SESSION['proyectos'];
+
+    // SE BUSCA POR ID Y SE ELIMINA DEL ARRAY
+    foreach ($proyectos as $key => $proyecto) {
+        if ($proyecto['id'] == $proyectoId) {
+            unset($proyectos[$key]);
+            break;
+        }
+    }
+    // ACTUALIZA EL ARRAY DE PROYECTOS EN LA SESION 
+    $_SESSION['proyectos'] = array_values($proyectos);
+}
+//FUNCION PARA SACAR EL ID DE UN PROYECTO
+function proyectoPorId($proyectoId){
+
+    $proyectos = $_SESSION['proyectos'];
+    
+    //BUSCA EL PROYECTO ID
+        foreach ($proyectos as $proyecto) {
+            if ($proyecto['id'] == $proyectoId) {
+               return $proyecto;
+            }
+        }
+}
 
 function generarPDF($proyectos) {
-    
 //NUEVO PDF, AÑADIMOS PAGINA Y ESTABLECEMOS LA FUENTE, TAMAÑO Y TIPO
     $pdf = new TCPDF();
     $pdf->AddPage();
