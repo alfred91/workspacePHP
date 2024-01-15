6bb0ccd8480c771c67f5d4aa008ce4503a4fdd51
alfred91 <alfred.91.mc@gmail.com>
2024-01-16 00:15:02 +0100

    .

--- PokemonAWS/Pokemon/index.js
+++ PokemonAWS/Pokemon/index.js
@@ -7,7 +7,7 @@ const pokemonRoutes = require("./routes/pokemonRoutes");
 
 app.use(
   cors({
-    origin: ["*"],
+    origin: ["http://localhost:8080", "http://localhost:3000"],
     methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
     credentials: true,
     exposedHeaders: ["Authorization"],
